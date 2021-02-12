b118b09ed449f4f7cf599f61c8064323e874d5f3
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-02-13 00:29:03 +0900

    회원 웹 기능 - 등록

--- src/main/java/member/memberspring/controller/MemberController.java
+++ src/main/java/member/memberspring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package member.memberspring.controller;
 
+import member.memberspring.domain.Member;
 import member.memberspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,22 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
