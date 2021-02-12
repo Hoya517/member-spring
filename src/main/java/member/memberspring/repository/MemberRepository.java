8d303647bd5b27a0dc0bd1c0e5ae0ec45236cdff
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-02-12 23:50:46 +0900

    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/member/memberspring/repository/MemberRepository.java
+++ src/main/java/member/memberspring/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package member.memberspring.repository;
 
 import member.memberspring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
