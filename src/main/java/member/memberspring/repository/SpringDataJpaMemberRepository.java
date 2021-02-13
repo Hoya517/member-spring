fcb2e6981de42206089c39413ede3f6bd80333a1
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-02-13 23:54:17 +0900

    스프링 데이터 JPA

---
+++ src/main/java/member/memberspring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package member.memberspring.repository;
+
+import member.memberspring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
