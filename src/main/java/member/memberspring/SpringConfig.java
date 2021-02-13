06e39f6ca12c05c611783527ae605c9784d97f8f
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-02-13 15:29:21 +0900

    순수 Jdbc

--- src/main/java/member/memberspring/SpringConfig.java
+++ src/main/java/member/memberspring/SpringConfig.java
@@ -1,13 +1,22 @@
 package member.memberspring;
 
+import member.memberspring.repository.JdbcMemberRepository;
 import member.memberspring.repository.MemberRepository;
 import member.memberspring.repository.MemoryMemberRepository;
 import member.memberspring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
