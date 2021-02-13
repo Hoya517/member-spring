53b65f664322655e51679a4d4e7ae29173787241
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-02-13 14:53:28 +0900

    H2 데이터베이스 설치

--- src/main/java/member/memberspring/controller/MemberController.java
+++ src/main/java/member/memberspring/controller/MemberController.java
@@ -4,8 +4,11 @@ import member.memberspring.domain.Member;
 import member.memberspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -27,10 +30,15 @@ public class MemberController {
         Member member = new Member();
         member.setName(form.getName());
 
-        System.out.println("member = " + member.getName());
-
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
